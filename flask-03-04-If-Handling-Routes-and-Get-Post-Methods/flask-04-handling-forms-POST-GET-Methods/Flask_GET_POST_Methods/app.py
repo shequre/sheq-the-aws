fcc81c3070a28d4591eb813c7891371f4ce0cfd4
shequre <shequre@gmail.com>
2023-05-15 22:20:13 +0300

    py

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
@@ -33,9 +33,9 @@ def calculate():
         num2 = request.form.get("number2")
         return render_template("result.html", result1 = num1, result2 = num2, lcm = lcm(int(num1),int(num2)), developer_name = 'Oktay')
     else:
-        return render_template("result.html", developer_name = "Oktay")
+        return render_template("result.html", developer_name = "none")
 
 # Add a statement to run the Flask application which can be debugged.
 if __name__== "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
